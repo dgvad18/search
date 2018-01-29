622080efe44014bc54b59779e1e0d370dbc455dd
Sory-Ken <syoryken@sina.com>
2018-01-29 09:27:07 +0800

    企典通搜索程序view
    企典通搜索程序view

---
+++ bin/script/common.js
@@ -0,0 +1,21 @@
+(function($, global){
+	var Native = {};
+	
+	Native.dialog = {
+		msg: function(m, t){
+			layer.msg(m, {
+				time: t
+			});
+		},
+		load: function(){
+			return layer.load(2, {shade: [0.2, '#000']})
+		},
+		close: function(e, parent){
+			if(!!parent) return parent.layer.close(e);
+			return layer.close(e);
+		}
+	};
+	
+	
+	global.Native = Native;
+}(jQuery, window));
