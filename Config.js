875f964a24112a79753583b5949b3d1380525914
Sory-Ken <syoryken@sina.com>
2018-02-05 10:15:01 +0800

    02-05日进度
    02-05日进度

--- Config.js
+++ Config.js
@@ -1,17 +1,19 @@
 "use strict";
+const path = require("path");
 
 module.exports = {
     "CACHE_SETTING":{
-        "PATH": process.cwd() + '/Runtime',
+        "PATH": __dirname + '/Runtime/Cache',
         "APPEND_STR": "biaou_search"
     },
+    "DB_TYPE": "mssql",
     "DB_SETTING":{
-        "user": "sydzsw",
-        "password": "kuku2013",
-        "server": "117.135.131.177",
-        "database": "sydzsw",
-        "options": {
-            "encrypt": true
-        }
+        user: 'sydzsw',
+        password: 'kuku2011',
+        server: '117.135.131.177',
+        database: 'sydzsw'
+    },
+    "NATIVE_DB_SETTING": {
+        DB_PATH: __dirname + '/biaou_search.sqlite'
     }
 };
